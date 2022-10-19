17bbe4a3bc348d5107d76937fc63b319408cc099
mrodriguez <mrodriguez@commvault.com>
2022-10-19 00:29:01 -0300

    Add back to home after submission

--- src/components/pages/Feedback/index.jsx
+++ src/components/pages/Feedback/index.jsx
@@ -1,6 +1,7 @@
 import { Alert, Button, TextField, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export const SendFeedback = () => {
   const [success, setSuccess] = useState(false);
@@ -26,43 +27,62 @@ export const SendFeedback = () => {
           ¡Gracias por ayudarnos a mejorar!🙂{" "}
         </Alert>
       )}
-      <Typography variant="h5" gutterBottom alignSelf="center" color="primary">
-        ¿Cómo podemos mejorar?
-      </Typography>
-      <form
-        name="feedback"
-        method="POST"
-        data-netlify="true"
-        action="/feedback/?success=true"
-      >
-        <input type="hidden" name="form-name" value="feedback" />
-        <TextField
-          margin="dense"
-          name="name"
-          fullWidth
-          id="name"
-          label="Nombre"
-          autoFocus
-        />
-        <TextField
-          margin="dense"
-          name="message"
-          required
-          fullWidth
-          id="message"
-          label="Mensaje"
-          multiline
-          minRows={5}
-        />
+      {!success ? (
+        <>
+          <Typography
+            variant="h5"
+            gutterBottom
+            alignSelf="center"
+            color="primary"
+          >
+            ¿Cómo podemos mejorar?
+          </Typography>
+          <form
+            name="feedback"
+            method="POST"
+            data-netlify="true"
+            action="/feedback/?success=true"
+          >
+            <input type="hidden" name="form-name" value="feedback" />
+            <TextField
+              margin="dense"
+              name="name"
+              fullWidth
+              id="name"
+              label="Nombre"
+              autoFocus
+            />
+            <TextField
+              margin="dense"
+              name="message"
+              required
+              fullWidth
+              id="message"
+              label="Mensaje"
+              multiline
+              minRows={5}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Enviar
+            </Button>
+          </form>
+        </>
+      ) : (
         <Button
-          type="submit"
+          component={Link}
+          to="/"
           fullWidth
           variant="contained"
           sx={{ mt: 3, mb: 2 }}
         >
-          Enviar
+          Volver al inicio
         </Button>
-      </form>
+      )}
     </Container>
   );
 };
